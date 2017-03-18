e6863dbc1be0ea6fa2abd40d2595f1a4d0287673
BoShurik <boshurik@gmail.com>
2017-03-18 21:01:48 +0300

    Add priority attribute for command tag

--- DependencyInjection/Compiler/CommandCompilerPass.php
+++ DependencyInjection/Compiler/CommandCompilerPass.php
@@ -20,12 +20,37 @@ class CommandCompilerPass implements CompilerPassInterface
     {
         $pool = $container->getDefinition('bo_shurik_telegram_bot.command_pool');
 
-        foreach ($container->findTaggedServiceIds('bo_shurik_telegram_bot.command') as $id => $tags) {
-            foreach ($tags as $tag) {
-                $pool->addMethodCall('addCommand', array(
-                    new Reference($id),
-                ));
+        $commands = $this->findAndSortTaggedServices('bo_shurik_telegram_bot.command', $container);
+        foreach ($commands as $command) {
+            $pool->addMethodCall('addCommand', array(
+                $command,
+            ));
+        }
+    }
+
+    /**
+     * From PriorityTaggedServiceTrait as we support symfony >= 2.7
+     *
+     * @param string $tagName
+     * @param ContainerBuilder $container
+     * @return array|mixed
+     */
+    private function findAndSortTaggedServices($tagName, ContainerBuilder $container)
+    {
+        $services = array();
+
+        foreach ($container->findTaggedServiceIds($tagName) as $serviceId => $tags) {
+            foreach ($tags as $attributes) {
+                $priority = isset($attributes['priority']) ? $attributes['priority'] : 0;
+                $services[$priority][] = new Reference($serviceId);
             }
         }
+
+        if ($services) {
+            krsort($services);
+            $services = call_user_func_array('array_merge', $services);
+        }
+
+        return $services;
     }
 }
